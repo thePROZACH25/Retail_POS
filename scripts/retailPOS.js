0790ac94580548e3bd3f3257dd3c5c0ffb949c89
Zach S.L <zach.s.ledford@gmail.com>
2020-07-08 22:16:02 -0400

    seeded the transactions database with tester obj

--- scripts/retailPOS.js
+++ scripts/retailPOS.js
@@ -76,6 +76,24 @@ const customerSeed = [
   },
 ];
 
+const transactionSeed = [
+  {
+    dateAdded: new Date(Date.now()),
+    transNum: "",
+    employeeNum: employeeSeed[0].employeeNum,
+    items: [
+      {
+        name: "",
+        price: "",
+      }
+    ],
+    subTotal: 0,
+    tax: 9.75,
+    total: 0,
+    payment: "",
+  },
+];
+
 db.Employee.remove({})
   .then(() => db.Employee.collection.insertMany(employeeSeed))
   .then((data) => {
@@ -87,8 +105,19 @@ db.Employee.remove({})
     process.exit(1);
   });
 
-  db.Customer.remove({})
+db.Customer.remove({})
   .then(() => db.Customer.collection.insertMany(customerSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+db.Transaction.remove({})
+  .then(() => db.Transaction.collection.insertMany(transactionSeed))
   .then((data) => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
