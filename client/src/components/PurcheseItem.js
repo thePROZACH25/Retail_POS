25987323008c443b97b3a17c6e69f37f28966479
Zach S.L <zach.s.ledford@gmail.com>
2020-07-15 22:05:54 -0400

    adding purchese item component

--- client/src/components/PurcheseItem.js
+++ client/src/components/PurcheseItem.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function PurcheseItem() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs>
+          <Paper className={classes.paper}>Random Product Info</Paper>
+        </Grid>
+      </Grid>
+      
+    </div>
+  );
 }
 
-export default PurcheseItem
+export default PurcheseItem;
